a9b4762ed92350249156bc4aa8a1150e746c40e5
Albin Tenghagen <albin.tenghagen@chasacademy.se>
2025-02-08 08:25:33 +0100

    Merge conflicts resolved

--- vite-folder/src/components/TravelFormFolder/TravelForm.jsx
+++ vite-folder/src/components/TravelFormFolder/TravelForm.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import "../TravelFormFolder/travelForm.css"; // Import the CSS file
 
-function TravelForm() {
-  const [travelArray, setTravelArray] = useState([]);
+function TravelForm({ handleSubmitTravel }) {
   const [formData, setFormData] = useState({
     country: "",
     timeOfDeparture: "",
@@ -22,18 +21,14 @@ function TravelForm() {
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
-
-  const handleSubmit = (e) => {
+  function formSubmit(e) {
     e.preventDefault();
-    setTravelArray({ travelArray, formData });
-    console.log("Form Submitted:", formData);
-    console.log("travelArray", travelArray);
-  };
-
+    handleSubmitTravel(formData);
+  }
   return (
     <div className="travel-form-container">
       <h2 className="travel-form-title">Travel Booking Form</h2>
-      <form onSubmit={handleSubmit} className="travel-form">
+      <form onSubmit={formSubmit} className="travel-form">
         {Object.keys(formData).map((key, index) => (
           <div
             key={key}
