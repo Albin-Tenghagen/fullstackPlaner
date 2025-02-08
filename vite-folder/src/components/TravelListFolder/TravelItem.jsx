66a14b34f78dbd419b6d9d7aec7459177cd0ffb4
Albin Tenghagen <albin.tenghagen@chasacademy.se>
2025-02-08 09:11:11 +0100

    With .replace() the T in time of departure has been replaced

--- vite-folder/src/components/TravelListFolder/TravelItem.jsx
+++ vite-folder/src/components/TravelListFolder/TravelItem.jsx
@@ -1,6 +1,6 @@
 // The travelItem component that renders the details of a single TravelItem
 //* @param {Travel} prop - the Travel details passed as a prop
-
+//TODO the number of activites is still static and needs to be altered in such a way that the number of activities is actually displayed and not just a p tag
 const TravelItem = ({
   country,
   timeOfDeparture,
@@ -11,7 +11,7 @@ const TravelItem = ({
   return (
     <article className="TravelItem">
       <h4>{country}</h4>
-      <p>{timeOfDeparture}</p>
+      <p>{timeOfDeparture.replace("T", " ")}</p>
       <p>{adventuresEnd}</p>
       <p>{travellingParty}</p>
       <p>{methodOfTransportation}</p>
