b6bc9fc69bad1ddd60d752f7fad701ee6a16f07f
Albin Tenghagen <albin.tenghagen@chasacademy.se>
2025-02-18 12:04:17 +0100

    details page is now using dynamic routing

--- vite-folder/src/pages/Details.jsx
+++ vite-folder/src/pages/Details.jsx
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
 import Modal from "../components/DetailsFolder/modal"; // Importera modal-komponenten
 import { useParams, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import TravelItem from "../components/TravelListFolder/TravelItem";
 function Details() {
   const { id } = useParams();
+  const travels = useSelector((state) => state.travel.travels);
+  const travel = travels.find((travel) => String(travel.id) === id);
 
   return (
     <>
       <p>Details Page suckaaaas</p>
       <section>
         <h2>Travel Details</h2>
-        <p>travelId: {id}</p>
+        <p>travel id: {id}</p>
+        {travel ? (
+          <TravelItem key={travel.id} travel={travel} />
+        ) : (
+          <h4>Hejsan hoppsan</h4>
+        )}
       </section>
     </>
   );
